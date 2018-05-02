2c2ad6598364eda5e6c5c34796e716a4ccd5b5c3
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2018-05-02 16:48:51 +0100

    set up initial scss infrastructure. test against prod.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, password_length: 12..128
+         :recoverable, :rememberable, :trackable, :validatable, password_length: 6..128
 end
