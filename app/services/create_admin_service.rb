2dd52577d2d3e881ef05b735c70b14c4f6003e50
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2018-05-02 18:22:40 +0100

    pass specs

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,8 +1,9 @@
 class CreateAdminService
   def call
     user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-        user.password = Rails.application.secrets.admin_password
-        user.password_confirmation = Rails.application.secrets.admin_password
+        pword = SecureRandom.hex(14)
+        user.password = pword
+        user.password_confirmation = pword
       end
   end
 end
