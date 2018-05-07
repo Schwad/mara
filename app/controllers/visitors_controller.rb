ec4f0abe1ef00a5b66384b7c66cac091eca6c2bf
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2018-05-07 13:45:46 +0100

    adds and regenerates documentation

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,10 @@
+##
+# This class handles public-facing GET requests on any web-ui elements built for the application.
+
 class VisitorsController < ApplicationController
+
+  ##
+  # The root path of the application web-ui.
+  
+  def index; end
 end
