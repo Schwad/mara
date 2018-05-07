28e71ae8ef8c7270eb44972963ace4da0445cb76
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2018-05-07 14:13:49 +0100

    removes unneeded service and updates documentation

--- app/services/create_admin_service.rb
+++
@@ -1,9 +0,0 @@
-class CreateAdminService
-  def call
-    user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
-        pword = SecureRandom.hex(14)
-        user.password = pword
-        user.password_confirmation = pword
-      end
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
