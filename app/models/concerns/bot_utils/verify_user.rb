64705d96cac60ce359cc3d628093358b3d030beb
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2018-05-08 23:34:10 +0100

    checks user verification

---
+++ app/models/concerns/bot_utils/verify_user.rb
@@ -0,0 +1,26 @@
+module BotUtils
+
+  ##
+  # This is utilized by MaraBot to ensure that it only communicates with existing users, by checking against current friendly names.
+  module VerifyUser
+    extend ActiveSupport::Concern
+
+    included do
+
+      ##
+      # Verifies current user, if not it kills any checks and also supports ratelimiting.
+      #
+      def verify_user(message)
+        puts 'Verifying...'
+        message.reply do |reply|
+          if message.from.username == 'schwadererer'
+            reply.text = 'yo playa'
+          else
+            reply.text = 'mmmmno wai'
+          end
+          reply.send_with(@bot)
+        end
+      end
+    end
+  end
+end
--- app/models/mara_bot.rb
+++ app/models/mara_bot.rb
@@ -6,6 +6,7 @@
 
 class MaraBot
   include Sidekiq::Worker
+  include BotUtils::VerifyUser
 
   MARA_BOT_KEY = ENV['mara_bot_key']
 
@@ -19,22 +20,23 @@ class MaraBot
 
   ##
   # Runs the bot, it must always be running 'live' on its own process/worker in
-  # order to receive updates.
+  # order to receive updates. This is done via the Procfile.
   def perform
     @bot.get_updates(fail_silently: true) do |message|
-      puts "@#{message.from.username}: #{message.text}"
-      command = message.get_command_for(@bot)
-
-      message.reply do |reply|
-        case command
-        when /greet/i
-          reply.text = "Hello, #{message.from.first_name}!"
-        else
-          reply.text = "#{message.from.first_name}, have no idea what #{command.inspect} means."
-        end
-        puts "sending #{reply.text.inspect} to @#{message.from.username}"
-        reply.send_with(@bot)
-      end
+      verify_user(message)
+      # puts "@#{message.from.username}: #{message.text}"
+      # command = message.get_command_for(@bot)
+      #
+      # message.reply do |reply|
+      #   case command
+      #   when /greet/i
+      #     reply.text = "Hello, #{message.from.first_name}!"
+      #   else
+      #     reply.text = "#{message.from.first_name}, have no idea what #{command.inspect} means."
+      #   end
+      #   puts "sending #{reply.text.inspect} to @#{message.from.username}"
+      #   reply.send_with(@bot)
+      # end
     end
   end
 end
