9149aa866c31c3cac54ccfaea83ee84cabe816ff
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2018-05-31 21:16:31 +0100

    Reformats data_point

--- app/models/data_point.rb
+++ app/models/data_point.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 ##
 # This stores the realtime pain data against weather. Often used data is accessed via the accessors associated with the `weather_data` store. A full snapshot of the weather for future use from Weather Underground is stored as a serialized hash in the `full_weather_record`.
 #
-
 class DataPoint < ApplicationRecord
   belongs_to :user
   store :weather_data, accessors: [ :pressure, :temperature ], coder: JSON
