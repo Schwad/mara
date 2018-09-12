211121e99882f9c713be0acf59df46ed6e8f1f7e
Nick Schwaderer <nick.schwaderer@oceanshq.com>
2018-09-12 16:08:14 +0100

    Ensures user supplies a numeric value

--- app/interactors/inspect_message_validity.rb
+++ app/interactors/inspect_message_validity.rb
@@ -8,12 +8,22 @@ class InspectMessageValidity
 
   def call
     puts 'Inspect message validity'
-    if ( context.incoming_message.to_f == 0.0 && context.incoming_message.to_f != '0' ) || context.incoming_message.to_f > 10.0
+    if non_integer_value || value_too_large
       context.message.reply do |reply|
-        reply.text = "hello #{context.message.from.username}, we are afraid that '#{context.incoming_message}' Is not a valid reply. Unless you are sending a valid command from the command list, please respond only with numbers from 0-10 so that they may be recorded. If something unexpected is happening here, get in touch with the maintainer at nicholas.schwaderer@gmail.com or on Telegram at @schwadererer."
+        reply.text = "Hello #{context.message.from.username}, we are afraid that '#{context.incoming_message}' Is not a valid reply. Unless you are sending a valid command from the command list, please respond only with numbers from 0-10 so that they may be recorded. If something unexpected is happening here, get in touch with the maintainer at nicholas.schwaderer@gmail.com or on Telegram at @schwadererer."
         reply.send_with(context.bot)
       end
       context.fail!
     end
+  end
+
+  private
+
+  def non_integer_value
+    ( context.incoming_message.to_f == 0.0 && context.incoming_message != '0' )
+  end
+
+  def value_too_large
+    context.incoming_message.to_f > 10.0
   end
 end
--- app/interactors/manage_message.rb
+++ app/interactors/manage_message.rb
@@ -6,5 +6,5 @@
 class ManageMessage
   include Interactor::Organizer
 
-  organize VerifyUser, UpdateUserLocation, HandleAnyCommands, CheckLocationStaleness, CheckTheWeather, RecordDataPoint, ThankTheUser
+  organize VerifyUser, UpdateUserLocation, HandleAnyCommands, InspectMessageValidity, CheckLocationStaleness, CheckTheWeather, RecordDataPoint, ThankTheUser
 end
