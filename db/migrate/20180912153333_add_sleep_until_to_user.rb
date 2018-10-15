f9d6d303494529a8630b5a82ec735a75540684f8
Nick Schwaderer <nick.schwaderer@oceanshq.com>
2018-10-15 21:14:12 +0100

    Default time.now on sleep_until

--- db/migrate/20180912153333_add_sleep_until_to_user.rb
+++ db/migrate/20180912153333_add_sleep_until_to_user.rb
@@ -1,5 +1,5 @@
 class AddSleepUntilToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :sleep_until, :datetime
+    add_column :users, :sleep_until, :datetime, default: DateTime.now
   end
 end
